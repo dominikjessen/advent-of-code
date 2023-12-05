3bbed1704ae1504ddfaa3560367acb2ed13c4c09
Dominik <djessen94@gmail.com>
2023-12-05 13:50:11 +0100

    [Day 5] Q2 (reverse brute force)

--- 2023/day5/day5.py
+++ 2023/day5/day5.py
@@ -1,6 +1,4 @@
 def build_range_bound_from_section(line: str) -> {}:
-  # Build array of range bounds for section
-  # [{'fromLowerBound': second_num, 'toLowerBound': second_num+third_num, 'toLowerBound' first_num, 'toUpperBound' first_num+third_num}]
   m = []
 
   mappings = [x for x in line.splitlines()[1:]]
@@ -22,9 +20,29 @@ def convert(value: int, bounds: {}) -> str:
     if bound['fromLowerBound'] <= value <= bound['fromUpperBound']:
       offset = bound['toLowerBound']-bound['fromLowerBound']
       return str(value+offset)
-  
-  # No mapping found, take original
+
   return str(value)
+
+def convert_reverse(value: int, bounds: {}) -> str:
+  for bound in bounds:
+    if bound['toLowerBound'] <= value <= bound['toUpperBound']:
+      offset = bound['fromLowerBound']-bound['toLowerBound']
+      return str(value+offset)
+    
+  return str(value)
+
+def make_seed_ranges(s: []) -> []:
+  seedRanges = []
+  start = 0
+  length = 1
+
+  while length < len(s):
+    from_to = {'from': int(s[start]), 'to': int(s[start])+int(s[length])}
+    seedRanges.append(from_to)
+    start += 2
+    length += 2
+  
+  return seedRanges
 
 def part_one(inp: str):
   sections = inp.split('\n\n')
@@ -39,13 +57,38 @@ def part_one(inp: str):
     for m in maps:
       mapped = int(convert(mapped,m))
     locations.append(mapped)
-  
-  print('Min location is:', min(locations))
+
+  print('Min location 1 is:', min(locations))
+
+# This is a reverse location -> seed, check if valid seed solution
+def part_two(inp: str):
+  sections = inp.split('\n\n')
+  seeds = sections[0].split(': ')[1].split()
+  seedRanges = make_seed_ranges(seeds)
+
+  # Build range bound maps: [seed -> soil -> fertilizer -> water -> light -> temperature -> humidity -> location]
+  maps = [build_range_bound_from_section(sections[1]),build_range_bound_from_section(sections[2]),build_range_bound_from_section(sections[3]),build_range_bound_from_section(sections[4]),build_range_bound_from_section(sections[5]),build_range_bound_from_section(sections[6]),build_range_bound_from_section(sections[7])]
+
+  # Manually parallelize by adjusting range bounds
+  for l in range(0, 1):
+    mapped = l
+    stepsLeft = len(maps)-1
+    while stepsLeft >= 0:
+      mapped = int(convert_reverse(mapped, maps[stepsLeft]))
+      stepsLeft -= 1
+    
+    # print('checking location ' + str(l) + ' with seed ' + str(mapped))
+    for sr in seedRanges:
+      # Edge case that first run has min seed
+      if sr['from'] <= mapped <= sr['to']:
+        print('mapped seed is in seeds: ', mapped)
+        print('Min location 2 is: ', l)
+        return
 
 
 # Input
-
 example = open('./example.txt').read().strip()
 inp = open('./input.txt').read().strip()
 
 part_one(inp)
+part_two(inp)
