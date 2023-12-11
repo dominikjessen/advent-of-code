45bc467306982247c355abbefa2dbf92be1722f3
Dominik <djessen94@gmail.com>
2023-12-11 13:19:17 +0100

    [Day 10] Flood fill approach

--- 2023/day10/day10.py
+++ 2023/day10/day10.py
@@ -1,6 +1,9 @@
 # Tricky part here is to remember to switch directions when curved pipe is encountered
 # NOTE: Because input only has 2 possible paths from S (i.e. both sides of same loop), just walk along one until back to S. 
 # NOTE: Else, would have to follow one path first until impossible move
+from collections import deque
+
+
 def part_one(inp: str):
   grid = []
   MOVES = {'n': (-1,0), 'e': (0,1), 's':(1,0), 'w':(0,-1)}
@@ -149,9 +152,101 @@ def part_two(inp: str):
     
   print('Answer 2 is:', ans)
 
+# This solution expands every tile into a 3x3 representation of 
+# E.g. I -> .#.
+#           .#.
+#           .#.
+# This way loop actually has containing areas of non-zero size
+def solve_with_flood_fill(inp: str):
+  EXPAND_SYMBOL = {
+    '|': '.#..#..#.',
+    '-': '...###...',
+    'L': '.#..##...',
+    'J': '.#.##....',
+    '7': '...##..#.',
+    'F': '....##.#.',
+    '.': '.........',
+  }
+
+  # Build grid and find S
+  grid = []
+  start = ()
+  for i,line in enumerate(inp.splitlines()):
+    row = []
+    for j,c in enumerate(list(line)):
+      if c == 'S':
+        start = (i,j)
+      row.append(c)
+    grid.append(row)  
+
+  # Manually replace S with correct connector pipe for input
+  # NOTE: Could find this generally
+  grid[start[0]][start[1]] = 'J'
+
+  G = [["" for _ in range(len(grid[0]) * 3)] for _ in range(len(grid) * 3)]
+  for r in range(len(grid)):
+    for c in range(len(grid[i])):
+      char = grid[r][c]
+      for dr in range(3):
+        for dc in range(3):
+          G[r*3+dr][c*3+dc] = EXPAND_SYMBOL[char][dr * 3 + dc]
+
+  # Use the expanded grid to find the loop
+  r, c = start[0] * 3 + 1, start[1] * 3 + 1
+  
+  Q = deque()
+  Q.append((r, c))
+  seen = set()
+
+  while Q:
+    r, c = Q.popleft()
+    # Check n, e, s, w
+    for dr, dc in [(-1, 0), (0, 1), (1, 0), (0, -1)]:
+        rr, cc = r + dr, c + dc
+        if 0 <= rr < len(G) and 0 <= cc < len(G[0]) and (rr, cc) not in seen and G[rr][cc] == "#":
+            Q.append((rr, cc))
+            seen.add((rr, cc))
+
+  print("Solution part 1:", len(seen) // 3 // 2)
+
+  # Part 2 - Remove every pipe piece not on loop
+  for r in range(len(G)):
+    for c in range(len(G[0])):
+        if (r, c) not in seen:
+            G[r][c] = "."
+
+  Q = deque()
+  Q.append((0, 0))
+  seen = set()
+
+  while Q:
+      r, c = Q.popleft()
+      G[r][c] = ' ' # Outside loop points
+
+      for dr, dc in [(-1, 0), (0, 1), (1, 0), (0, -1)]:
+          rr, cc = r + dr, c + dc
+          if 0 <= rr < len(G) and 0 <= cc < len(G[0]) and (rr, cc) not in seen and G[rr][cc] == ".":
+              Q.append((rr, cc))
+              seen.add((rr, cc))
+  
+  # Visual of graph
+  for row in G:
+    print(''.join(row))
+
+  p2 = 0
+  for r in range(len(grid)):
+      for c in range(len(grid[r])):
+          p2 += 1 if all(G[r * 3 + dr][c * 3 + dc] == "." for dr in range(3) for dc in range(3)) else 0
+  
+  print("Solution part 2:", p2)
+
 # Input
 i = open('./input.txt').read().strip()
 e1 = open('./example1.txt').read().strip()
+e2 = open('./example2.txt').read().strip()
+e3 = open('./example3.txt').read().strip()
+e4 = open('./example4.txt').read().strip()
+
 
 part_one(i)
-part_two(i)
+solve_with_flood_fill(i)
