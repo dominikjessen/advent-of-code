a65c84840faf3f9249ff615f14f58c799aa35b8d
Dominik <djessen94@gmail.com>
2023-12-15 16:53:56 +0100

    [Day 15] Q2

--- 2023/day15/day15.py
+++ 2023/day15/day15.py
@@ -17,9 +17,34 @@ def part_one(inp: str):
   
   print('Answer 1 is:', s)
 
+def part_two(inp: str):
+  sequences = inp.split(',')
+  boxes: list[dict[str,int]] = []
+
+  for _i in range(256):
+    boxes.append({})
+
+  for sequence in sequences:
+    if sequence.find('=') != -1:
+      [label,focal_len] = sequence.split('=')
+      box_idx = ascii_sum(label)
+      boxes[box_idx][label] = int(focal_len)
+    elif sequence.find('-') != -1:
+      label = sequence[:-1]
+      box_idx = ascii_sum(label)
+      if label in boxes[box_idx]:
+        del boxes[box_idx][label]
+
+  s = 0
+  for i,box in enumerate(boxes):
+    for j,v in enumerate(box.values()):
+      s += (i+1) * (j+1) * v
+
+  print('Answer 2 is:', s)
+
 # Input
 i = open('./input.txt').read().strip()
 e = open('./example.txt').read().strip()
 
 part_one(i)
-# part_two(i)
+part_two(i)
