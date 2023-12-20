9ed34721347d61e11f7664dfbb3632ff9be12aa4
Dominik <djessen94@gmail.com>
2023-12-20 20:22:18 +0100

    [Day 12] Q2 (Memoized)

--- 2023/day12/day12.py
+++ 2023/day12/day12.py
@@ -1,4 +1,4 @@
-def count_valid_permutations(springs: str, criteria: tuple[int, ...]) -> int:
+def count_valid_permutations(springs: str, criteria: tuple[int, ...], known_counts: dict[tuple[str,tuple[int,...]], int] = {}) -> int:
   # No more springs left is only valid if no more criteria to fit
   if springs == '':
     return 1 if criteria == () else 0
@@ -6,6 +6,11 @@ def count_valid_permutations(springs: str, criteria: tuple[int, ...]) -> int:
   # No more criteria left is only valid if no more blocks of springs left to fill -> remainder of string's ? all become . -> 1 permutation
   if criteria == ():
     return 0 if '#' in springs else 1
+  
+  k = (springs, criteria) # Memoize
+
+  if k in known_counts:
+    return known_counts[k]
   
   result = 0
 
@@ -21,6 +26,7 @@ def count_valid_permutations(springs: str, criteria: tuple[int, ...]) -> int:
       # Can place a valid block here and count remaining string (+1 space for gap between blocks)
       result += count_valid_permutations(springs[criteria[0]+1:], criteria[1:])
 
+  known_counts[k] = result
   return result
 
 def part_one(inp: str):
@@ -31,9 +37,23 @@ def part_one(inp: str):
     s += count_valid_permutations(springs, criteria)
 
   print('Answer 1 is:', s)
+
+def part_two(inp: str, fold_factor: int = 5):
+  s = 0
+  for line in inp.splitlines():
+    spr,c = line.split(' ')
+
+    # Unfold
+    springs = '?'.join([spr]*fold_factor)
+    criteria = tuple(map(int, c.split(',') * fold_factor))
+
+    s += count_valid_permutations(springs, criteria)
+
+  print('Answer 2 is:', s)
   
 # Input
 i = open('./input.txt').read().strip()
 e = open('./example.txt').read().strip()
 
 part_one(i)
+part_two(i)
