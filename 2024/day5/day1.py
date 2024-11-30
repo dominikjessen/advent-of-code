aa06dcc6dd066b79e5c8b487cb0f09dd1bf5fc37
Dominik <djessen94@gmail.com>
2024-11-30 12:26:30 +0100

    Prep 2024 setup

---
+++ 2024/day5/day1.py
@@ -0,0 +1,39 @@
+############
+#  Part 1  #
+############
+
+def part_one(inp: str):
+  print('Answer 1 is:')
+
+
+############
+#  Part 2  #
+############
+
+def part_two(inp: str):
+  print('Answer 2 is:')
+
+
+#############
+#  Solving  #
+#############
+
+# Input
+example = open('./example.txt').read().strip()
+inp = open('./input.txt').read().strip()
+
+# Solve example
+
+print('Example')
+print(40 * '=')
+
+part_one(example)
+part_two(example)
+
+# Solve input
+
+# print('\nSolution')
+# print(40 * '=')
+
+# part_one(inp)
+# part_two(inp)
