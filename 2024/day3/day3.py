a3033087d87f1b4c8cfe1e81ca06f159bbc7df7e
Dominik <djessen94@gmail.com>
2024-12-03 23:03:02 +0100

    [Day 3] Part 2

--- 2024/day3/day3.py
+++ 2024/day3/day3.py
@@ -12,13 +12,16 @@ from utils.utils import *
 #  Part 1  #
 ############
 
-def part_one(inp: str):
+def mul_sum(inp: str) -> int:
   matches = re.findall('mul\(\d{1,3},\d{1,3}\)', inp)
   s = 0
   for m in matches:
     s += functools.reduce(operator.mul, map(int,  m[4:-1].split(',')), 1)
+  
+  return s
 
-  print('Answer 1 is:', s)
+def part_one(inp: str):
+  print('Answer 1 is:', mul_sum(inp))
 
 
 ############
@@ -26,7 +29,13 @@ def part_one(inp: str):
 ############
 
 def part_two(inp: str):
-  print('Answer 2 is:')
+  test_str = ''
+  dos = inp.split("do()")
+
+  for d in dos:
+    test_str += d.split("don't()")[0]
+
+  print('Answer 2 is:', mul_sum(test_str))
 
 
 #############
@@ -35,6 +44,7 @@ def part_two(inp: str):
 
 # Input
 example = open('./example.txt').read().strip()
+example2 = open('./example2.txt').read().strip()
 inp = open('./input.txt').read().strip()
 
 # Solve example
@@ -43,7 +53,7 @@ print('Example')
 print(40 * '=')
 
 part_one(example)
-part_two(example)
+part_two(example2)
 
 # Solve input
 
@@ -51,4 +61,4 @@ print('\nSolution')
 print(40 * '=')
 
 part_one(inp)
-# part_two(inp)
+part_two(inp)
