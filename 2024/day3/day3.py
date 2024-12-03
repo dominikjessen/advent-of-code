bfe7deff51dc1fa380521eaeb52b7b8bfbbbc87c
Dominik <djessen94@gmail.com>
2024-12-03 07:41:12 +0100

    [Day 3] Part 1

--- 2024/day3/day3.py
+++ 2024/day3/day3.py
@@ -1,5 +1,8 @@
 import sys
 import os
+import re
+import operator
+import functools
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 
@@ -10,7 +13,12 @@ from utils.utils import *
 ############
 
 def part_one(inp: str):
-  print('Answer 1 is:')
+  matches = re.findall('mul\(\d{1,3},\d{1,3}\)', inp)
+  s = 0
+  for m in matches:
+    s += functools.reduce(operator.mul, map(int,  m[4:-1].split(',')), 1)
+
+  print('Answer 1 is:', s)
 
 
 ############
@@ -39,8 +47,8 @@ part_two(example)
 
 # Solve input
 
-# print('\nSolution')
-# print(40 * '=')
+print('\nSolution')
+print(40 * '=')
 
-# part_one(inp)
+part_one(inp)
 # part_two(inp)
