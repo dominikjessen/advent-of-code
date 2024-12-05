832c36c5b144622af5c8da9f34754f74ef2d3889
Dominik <djessen94@gmail.com>
2024-12-05 10:03:26 +0100

    [Day 5] Part 2

--- 2024/day5/day5.py
+++ 2024/day5/day5.py
@@ -1,5 +1,6 @@
 import sys
 import os
+from copy import deepcopy
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 
@@ -70,8 +71,54 @@ def part_one(inp: str):
 ############
 
 def part_two(inp: str):
-  print('Answer 2 is:')
+  r, o = inp.split('\n\n')
+  rules = {}
+  s = 0
 
+  # Make rule set
+  for line in get_input_rows(r):
+    bef, aft = line.split('|')
+
+    if not bef in rules:
+      rules[bef] = {
+        "is_before": [],
+        "is_after": []
+      }
+    
+    if not aft in rules:
+      rules[aft] = {
+        "is_before": [],
+        "is_after": []
+      }
+
+    rules[bef]["is_before"].append(aft)
+    rules[aft]["is_after"].append(bef)
+
+  # Find incorrect lines
+  incorrect = []
+  for line in get_input_rows(o):
+    if get_middle_number(line, rules) == 0:
+      incorrect.append(line)
+  
+  # Reorder incorrect lines
+  for line in incorrect:
+    orig = line.split(',')
+
+    # Remove all non-existent numbers from rules here
+    relevant_rules = deepcopy(rules)
+
+    for r in relevant_rules.values():
+      r["is_before"] = list(set(orig).intersection(r["is_before"])).copy()
+      r["is_after"] = list(set(orig).intersection(r["is_after"])).copy()
+
+    # Correct ordered position is how many num comes after
+    ordered = [None for _ in range(len(orig))]
+    for num in orig:
+      ordered[len(relevant_rules[num]["is_after"])] = num
+    
+    s += int(ordered[len(ordered)//2])
+
+  print('Answer 2 is:', s)
 
 #############
 #  Solving  #
@@ -95,4 +142,4 @@ print('\nSolution')
 print(40 * '=')
 
 part_one(inp)
-# part_two(inp)
+part_two(inp)
