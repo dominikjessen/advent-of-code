b675aa7c07827b142fcb21ffed0ea4a6f21e8390
Dominik <djessen94@gmail.com>
2024-12-06 09:03:30 +0100

    [Day 6] Part 1

--- 2024/day6/day6.py
+++ 2024/day6/day6.py
@@ -10,7 +10,45 @@ from utils.utils import *
 ############
 
 def part_one(inp: str):
-  print('Answer 1 is:')
+  G = get_input_grid_char(inp)
+
+  curr = ()
+  curr_dir_i = 0
+
+  directions = [
+    (-1, 0),  # Up
+    (0, 1),   # Right
+    (1, 0),   # Down
+    (0, -1),  # Left
+  ]
+
+  visited = set()
+
+  for r in range(len(G)):
+    for c in range(len(G[r])):
+      if G[r][c] == '^':
+        curr = (r, c)
+        visited.add((r,c))
+  
+  
+  while is_valid_move(G, curr[0], curr[1]):
+    dr,dc = directions[curr_dir_i]
+    nr,nc = curr[0] + dr, curr[1] + dc
+
+    if not is_valid_move(G, nr, nc):
+      break
+
+    if G[nr][nc] == '#':
+      curr_dir_i = (curr_dir_i + 1) % len(directions)
+      continue
+
+    if not (nr,nc) in visited:
+      G[nr][nc] = '@'
+      visited.add((nr,nc))
+
+    curr = (nr,nc)
+  
+  print('Answer 1 is:', len(visited))
 
 
 ############
@@ -39,8 +77,8 @@ part_two(example)
 
 # Solve input
 
-# print('\nSolution')
-# print(40 * '=')
+print('\nSolution')
+print(40 * '=')
 
-# part_one(inp)
+part_one(inp)
 # part_two(inp)
