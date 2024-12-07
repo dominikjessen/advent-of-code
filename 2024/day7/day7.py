eb6b966f77bd7ca840b8f502a0d461ba58faf48f
Dominik <djessen94@gmail.com>
2024-12-07 14:38:04 +0100

    [Day 7] Part 1

--- 2024/day7/day7.py
+++ 2024/day7/day7.py
@@ -10,7 +10,31 @@ from utils.utils import *
 ############
 
 def part_one(inp: str):
-  print('Answer 1 is:')
+  lines = get_input_rows(inp)
+  s = 0
+
+  for l in lines:
+    sol, nums = l.split(':')
+    sol = int(sol)
+    nums = [int(x) for x in nums.split()]
+
+    stack = []
+    stack.append((nums[0], 1))
+
+    while stack:
+      curr, i = stack.pop()
+
+      if curr == sol and i == len(nums):
+        s += sol
+        break
+
+      if i == len(nums):
+        continue
+
+      stack.append((curr + nums[i], i+1))
+      stack.append((curr * nums[i], i+1))
+
+  print('Answer 1 is:', s)
 
 
 ############
@@ -39,8 +63,8 @@ part_two(example)
 
 # Solve input
 
-# print('\nSolution')
-# print(40 * '=')
+print('\nSolution')
+print(40 * '=')
 
-# part_one(inp)
+part_one(inp)
 # part_two(inp)
