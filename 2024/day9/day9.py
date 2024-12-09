a705a92ea11f1a63eb74ca130c744b24f169a94b
Dominik <djessen94@gmail.com>
2024-12-09 21:02:56 +0100

    [Day 9] Part 1

--- 2024/day9/day9.py
+++ 2024/day9/day9.py
@@ -10,7 +10,46 @@ from utils.utils import *
 ############
 
 def part_one(inp: str):
-  print('Answer 1 is:')
+  disc = []
+  file_lengths = []
+  free_lengths = []
+
+  for i, num in enumerate(list(inp)):
+    if i % 2 == 0:
+      file_lengths.append(int(num))
+    else:
+      free_lengths.append(int(num))
+
+  left_pointer = 0
+  right_pointer = len(file_lengths) - 1
+  free_pointer = 0
+  writing_file = True
+
+  while left_pointer <= right_pointer:
+    if writing_file:
+      while file_lengths[left_pointer] > 0:
+        disc.append(left_pointer)
+        file_lengths[left_pointer] -= 1
+      
+      writing_file = False
+      left_pointer += 1
+    else:
+      if free_pointer >= len(free_lengths):
+        continue
+
+      while free_lengths[free_pointer] > 0:
+        if file_lengths[right_pointer] > 0:
+          disc.append(right_pointer)
+          free_lengths[free_pointer] -= 1
+          file_lengths[right_pointer] -= 1
+        else:
+          right_pointer -= 1
+
+      writing_file = True
+      free_pointer += 1
+
+  s =  sum(i * num for i, num in enumerate(disc))
+  print('Answer 1 is:', s)
 
 
 ############
@@ -39,8 +78,8 @@ part_two(example)
 
 # Solve input
 
-# print('\nSolution')
-# print(40 * '=')
+print('\nSolution')
+print(40 * '=')
 
-# part_one(inp)
+part_one(inp)
 # part_two(inp)
