2791b0791d610a0537acc211c2dad2b83c41e4d6
Dominik <djessen94@gmail.com>
2024-12-09 23:03:57 +0100

    [Day 7] Part 2

--- 2024/day9/day9.py
+++ 2024/day9/day9.py
@@ -57,7 +57,57 @@ def part_one(inp: str):
 ############
 
 def part_two(inp: str):
-  print('Answer 2 is:')
+  files = {}
+  frees = []
+
+  file_id = 0
+  pos = 0
+
+  # Find all files and non-empty blanks with positions and lengths
+  for i, num in enumerate(list(inp)):
+    l = int(num)
+    if i % 2 == 0:
+      if l == 0:
+        raise ValueError('unexpected 0 length for file')
+      
+      files[file_id] = (pos, l)
+      file_id += 1
+    else:
+      if l != 0:
+        frees.append((pos, l))
+    
+    pos += l
+  
+  # Build our moved file positions in the dict
+  while file_id > 0:
+    file_id -= 1
+    file_pos, file_size = files[file_id]
+
+    for i, (free_pos, free_size) in enumerate(frees):
+      # We have tried all free positions for this file and any free space to the right is irrelevant for us from here
+      if free_pos >= file_pos:
+        frees = frees[:i]
+        break
+      
+      # Move the file if space and handle remaining free space
+      if file_size <= free_size:
+        files[file_id] = (free_pos, file_size)
+
+        # File fits free space exactly --> remove free space from future contention
+        # Else adjust starting pos for free space and length of remaining free space by file_size
+        if file_size == free_size:
+          frees.pop(i)
+        else:
+          frees[i] = (free_pos + file_size, free_size - file_size)
+        
+        break
+
+  s = 0
+  for f_id, (start, size) in files.items():
+    for x in range(start, start + size):
+      s += x * f_id
+
+  print('Answer 2 is:', s)
 
 
 #############
@@ -82,4 +132,4 @@ print('\nSolution')
 print(40 * '=')
 
 part_one(inp)
-# part_two(inp)
+part_two(inp)
