a55b994abc31ca44169590969c68462f0ca457b6
Dominik <djessen94@gmail.com>
2024-12-11 17:51:29 +0100

    [Day 11] Part 1

--- 2024/day11/day11.py
+++ 2024/day11/day11.py
@@ -4,13 +4,41 @@ import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 
 from utils.utils import *
+from collections import defaultdict
+from typing import DefaultDict
 
 ############
 #  Part 1  #
 ############
 
+def blink(stones: DefaultDict[str, int]) -> DefaultDict[str, int]:
+  new_stones = defaultdict(int)
+
+  for stone, count in stones.items():
+    if stone == '0':
+        new_stones['1'] += count
+    elif len(stone) % 2 == 0:
+      l,r =  stone[:len(stone)//2], stone[len(stone)//2:]
+      r = str(int(r))
+
+      new_stones[l] += count
+      new_stones[r] += count
+    else:
+      new_stones[str(int(stone) * 2024)] += count
+
+  return new_stones
+
 def part_one(inp: str):
-  print('Answer 1 is:')
+  # Get initial stones and their counts
+  stones = defaultdict(int)
+
+  for s in inp.split():
+    stones[s] += 1
+  
+  for n in range(25):
+    stones = blink(stones)
+
+  print('Answer 1 is:', sum(stones.values()))
 
 
 ############
@@ -26,21 +54,28 @@ def part_two(inp: str):
 #############
 
 # Input
-example = open('./example.txt').read().strip()
+example1 = open('./example1.txt').read().strip()
+example2 = open('./example2.txt').read().strip()
 inp = open('./input.txt').read().strip()
 
 # Solve example
 
-print('Example')
+print('Example 1')
 print(40 * '=')
 
-part_one(example)
-part_two(example)
+part_one(example1)
+part_two(example1)
+
+# print('\nExample 2')
+# print(40 * '=')
+
+# part_one(example2)
+# part_two(example2)
 
 # Solve input
 
-# print('\nSolution')
-# print(40 * '=')
+print('\nSolution')
+print(40 * '=')
 
-# part_one(inp)
+part_one(inp)
 # part_two(inp)
