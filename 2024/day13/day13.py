ca80d8cdbf040ccd27351a498ed323e28d10c2c6
Dominik <djessen94@gmail.com>
2024-12-14 17:51:12 +0100

    [Day 13] Part 2

--- 2024/day13/day13.py
+++ 2024/day13/day13.py
@@ -36,7 +36,24 @@ def part_one(inp: str):
 ############
 
 def part_two(inp: str):
-  print('Answer 2 is:')
+    # This is just a system of linear equations with 2 unknowns
+  s = 0
+
+  machines = [x.split('\n') for x in inp.split('\n\n')]
+  for m in machines:
+    ax,ay = map(int, m[0].replace('Button A: X+', '').replace(', Y+', ',').split(','))
+    bx,by = map(int, m[1].replace('Button B: X+', '').replace(', Y+', ',').split(','))
+    px,py = map(int, m[2].replace('Prize: X=', '').replace(', Y=', ',').split(','))
+
+    A = np.array([[ax, bx],[ay, by]])
+    B = np.array([px, py]) + 10000000000000
+    S = np.round(np.linalg.solve(A,B))
+
+    # Verify solution with matrix multiplication
+    if (B == S @ A.T).all():
+      s += 3 * S[0] + S[1]
+  
+  print('Answer 2 is:', s)
 
 
 #############
@@ -61,4 +78,4 @@ print('\nSolution')
 print(40 * '=')
 
 part_one(inp)
-# part_two(inp)
+part_two(inp)
