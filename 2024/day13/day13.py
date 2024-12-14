b591e4a246856317f96445b7ed0c353d82a98607
Dominik <djessen94@gmail.com>
2024-12-14 16:46:50 +0100

    [Day 13] Part 1

--- 2024/day13/day13.py
+++ 2024/day13/day13.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import numpy as np
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 
@@ -10,7 +11,24 @@ from utils.utils import *
 ############
 
 def part_one(inp: str):
-  print('Answer 1 is:')
+  # This is just a system of linear equations with 2 unknowns
+  s = 0
+
+  machines = [x.split('\n') for x in inp.split('\n\n')]
+  for m in machines:
+    ax,ay = map(int, m[0].replace('Button A: X+', '').replace(', Y+', ',').split(','))
+    bx,by = map(int, m[1].replace('Button B: X+', '').replace(', Y+', ',').split(','))
+    px,py = map(int, m[2].replace('Prize: X=', '').replace(', Y=', ',').split(','))
+
+    A = np.array([[ax, bx],[ay, by]])
+    B = np.array([px, py])
+    a,b = np.linalg.solve(A,B)
+
+    # Account for floating point errors using numpy's built-in and check 100 heuristic we were given 
+    if 0 <= a <= 100 and 0 <= b <= 100 and np.isclose(a,round(a)) and np.isclose(b,round(b)):
+      s += 3 * a + b 
+
+  print('Answer 1 is:', s)
 
 
 ############
@@ -39,8 +57,8 @@ part_two(example)
 
 # Solve input
 
-# print('\nSolution')
-# print(40 * '=')
+print('\nSolution')
+print(40 * '=')
 
-# part_one(inp)
+part_one(inp)
 # part_two(inp)
