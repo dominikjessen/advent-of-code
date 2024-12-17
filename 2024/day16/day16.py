0b239f053f9fc91d9bdeb69c643d3fb6776e81da
Dominik <djessen94@gmail.com>
2024-12-17 12:09:19 +0100

    [Day 16] Part 2

--- 2024/day16/day16.py
+++ 2024/day16/day16.py
@@ -1,4 +1,3 @@
-import copy
 import heapq
 import sys
 import os
@@ -11,9 +10,7 @@ from utils.utils import *
 #  Part 1  #
 ############
 
-def part_one(inp: str):
-  # Dijkstra the shortest path --> potentially just do this because building graph might be expensive
-  
+def part_one(inp: str):  
   G = get_input_grid_char(inp)
   rows = len(G)
   cols = len(G[0])
@@ -57,7 +54,7 @@ def part_one(inp: str):
       if dr == cdr and dc == cdc:
         heapq.heappush(pq, ( curr_score + 1, r + dr, c + dc, dr, dc))
       else:
-        heapq.heappush(pq, ( curr_score + 1001, r + dr, c + dc, dr, dc)) # Just turn
+        heapq.heappush(pq, ( curr_score + 1001, r + dr, c + dc, dr, dc)) # Turn and take 1 step
 
   print('Answer 1 is:', scores[end])
 
@@ -67,7 +64,62 @@ def part_one(inp: str):
 ############
 
 def part_two(inp: str):
-  print('Answer 2 is:')
+  G = get_input_grid_char(inp)
+  rows = len(G)
+  cols = len(G[0])
+  start = ()
+  end = ()
+
+  for r in range(rows):
+    for c in range(cols):
+      if G[r][c] == 'S':
+        start = (r,c)
+      elif G[r][c] == 'E':
+        end = (r,c)
+  
+  scores = {}
+  seen = {}
+  seats = set()
+
+  # Start facing east
+  pq = [(0, start[0], start[1], 0, 1, set([start]))]
+
+  while pq:
+    curr_score, r, c, cdr, cdc, c_seats = heapq.heappop(pq)
+    c_seats.add((r,c))
+
+    if not (r,c) in scores:
+      scores[(r,c)] = curr_score
+
+      if (r,c) == end:
+        seats = c_seats
+    elif scores[(r,c)] >= curr_score:
+      scores[(r,c)] = curr_score
+
+      if (r,c) == end:
+        seats = seats.union(c_seats)
+
+    # Consider paths of same length to end
+    if (r,c,cdr,cdc) in seen:
+      if seen[(r,c,cdr,cdc)] < curr_score:
+        continue
+    
+    seen[(r,c,cdr,cdc)] = curr_score
+
+    # Can't turn around so remove that direction from valid
+    valid_dirs = set([(-1, 0), (0, 1), (1, 0), (0, -1)])
+    valid_dirs.remove((-cdr, -cdc))
+
+    for (dr,dc) in valid_dirs:
+      if G[r+dr][c+dc] == '#':
+        continue
+
+      if dr == cdr and dc == cdc:
+        heapq.heappush(pq, ( curr_score + 1, r + dr, c + dc, dr, dc, c_seats.copy()))
+      else:
+        heapq.heappush(pq, ( curr_score + 1000, r, c, dr, dc, c_seats.copy())) # Turn
+  
+  print('Answer 2 is:', len(seats))
 
 
 #############
@@ -76,7 +128,6 @@ def part_two(inp: str):
 
 # Input
 example = open('./example.txt').read().strip()
-example2 = open('./example2.txt').read().strip()
 inp = open('./input.txt').read().strip()
 
 # Solve example
@@ -85,8 +136,7 @@ print('Example')
 print(40 * '=')
 
 part_one(example)
-part_one(example2)
-# part_two(example)
+part_two(example)
 
 # Solve input
 
@@ -94,4 +144,4 @@ print('\nSolution')
 print(40 * '=')
 
 part_one(inp)
-# part_two(inp)
+part_two(inp)
