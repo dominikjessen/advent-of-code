9a6f88d38f6ea9125afafba12e34539e7f333db1
Dominik <djessen94@gmail.com>
2024-12-17 11:45:27 +0100

    [Day 16] Part 1

--- 2024/day16/day16.py
+++ 2024/day16/day16.py
@@ -1,9 +1,65 @@
+import copy
+import heapq
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+
+from utils.utils import *
+
 ############
 #  Part 1  #
 ############
 
 def part_one(inp: str):
-  print('Answer 1 is:')
+  # Dijkstra the shortest path --> potentially just do this because building graph might be expensive
+  
+  G = get_input_grid_char(inp)
+  rows = len(G)
+  cols = len(G[0])
+  start = ()
+  end = ()
+
+  for r in range(rows):
+    for c in range(cols):
+      if G[r][c] == 'S':
+        start = (r,c)
+      elif G[r][c] == 'E':
+        end = (r,c)
+  
+  scores = {}
+  seen = set()
+
+  # Start facing east
+  pq = [(0, start[0], start[1], 0, 1)]
+
+  while pq:
+    curr_score, r, c, cdr, cdc = heapq.heappop(pq)
+
+    if not (r,c) in scores:
+      scores[(r,c)] = curr_score
+    elif scores[(r,c)] > curr_score:
+      scores[(r,c)] = curr_score
+
+    if (r,c,cdr,cdc) in seen:
+      continue
+    
+    seen.add((r,c,cdr,cdc))
+
+    valid_dirs = set([(-1, 0), (0, 1), (1, 0), (0, -1)])
+    # Can't turn around so remove that direction from valid
+    valid_dirs.remove((-cdr, -cdc))
+
+    for (dr,dc) in valid_dirs:
+      if G[r+dr][c+dc] == '#':
+        continue
+
+      if dr == cdr and dc == cdc:
+        heapq.heappush(pq, ( curr_score + 1, r + dr, c + dc, dr, dc))
+      else:
+        heapq.heappush(pq, ( curr_score + 1001, r + dr, c + dc, dr, dc)) # Just turn
+
+  print('Answer 1 is:', scores[end])
 
 
 ############
@@ -20,6 +76,7 @@ def part_two(inp: str):
 
 # Input
 example = open('./example.txt').read().strip()
+example2 = open('./example2.txt').read().strip()
 inp = open('./input.txt').read().strip()
 
 # Solve example
@@ -28,12 +85,13 @@ print('Example')
 print(40 * '=')
 
 part_one(example)
-part_two(example)
+part_one(example2)
+# part_two(example)
 
 # Solve input
 
-# print('\nSolution')
-# print(40 * '=')
+print('\nSolution')
+print(40 * '=')
 
-# part_one(inp)
+part_one(inp)
 # part_two(inp)
