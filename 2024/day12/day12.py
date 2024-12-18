c6cf1e6380194b51209bc3d8bb824e816f4e475c
Dominik <djessen94@gmail.com>
2024-12-18 21:54:52 +0100

    [Day 12] Part 2

--- 2024/day12/day12.py
+++ 2024/day12/day12.py
@@ -1,6 +1,5 @@
 import sys
 import os
-import numpy as np
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 
@@ -9,9 +8,6 @@ from utils.utils import *
 ############
 #  Part 1  #
 ############
-
-def shoelace_area(x, y) -> int:
-    return 0.5 * np.abs(np.dot(x, np.roll(y, 1)) - np.dot(y, np.roll(x, 1)))
 
 def get_cell_perimeter(G: List[List[str]], cell: Tuple[int,int]) -> int:
   p = 4
@@ -89,8 +85,103 @@ def part_one(inp: str):
 #  Part 2  #
 ############
 
+def get_component_sides(G: List[List[int]], comps: Set[Tuple[int,int]]) -> int:
+  # Build edges between garden cells first (i.e. fence pieces) which have an orientation toward their garden (think: walls)
+  edges = {}
+
+  for r,c in comps:
+    for nr,nc in get_adjacent_coords(r,c):
+      if (nr,nc) in comps:
+        continue
+        
+      er = (r + nr) / 2
+      ec = (c + nc) / 2
+      edges[(er, ec)] = (er - r, ec - c)
+  
+  # Adjacent fence pieces with the same direction in respect to the garden they surround are one side
+  seen = set()
+  sides = 0
+
+  for edge, e_dir in edges.items():
+    if edge in seen:
+      continue
+
+    seen.add(edge)
+    sides += 1
+
+    er, ec = edge
+
+    # Advance forward with our side edge or other edges by stepping until direction change
+    if er % 1 == 0:
+      for dr in [-1,1]:
+        cr = er + dr
+        while edges.get((cr, ec)) == e_dir:
+          seen.add((cr, ec))
+          cr += dr
+    else:
+      for dc in [-1,1]:
+        cc = ec + dc
+        while edges.get((er, cc)) == e_dir:
+          seen.add((er, cc))
+          cc += dc
+  return sides
+
 def part_two(inp: str):
-  print('Answer 2 is:')
+  G = get_input_grid_char(inp)
+  components = []
+
+  # Make sure to have new entry points for stack after exhausting component
+  unvisited = set()
+  for r in range(len(G)):
+    for c in range(len(G[r])):
+      unvisited.add((r,c))
+  
+  # Get connected components and their coords
+  stack = []
+  stack.append((0, 0, G[0][0], set()))
+  seen = set()
+
+  while stack:
+    r, c, char, component_parts = stack.pop()
+
+    # We've found the end of a garden plot, add it to components and restart if still unvisited cells left
+    if not stack:
+      components.append(component_parts)
+  
+      if unvisited:
+        ur,uc = unvisited.pop()
+        stack.append((ur,uc, G[ur][uc], set()))
+
+    if (r,c) in seen:
+      continue
+
+    unvisited.discard((r,c))
+    component_parts.add((r,c))
+    seen.add((r,c))
+
+    directions = [
+        (-1, 0),  # Up
+        (0, 1),   # Right
+        (1, 0),   # Down
+        (0, -1),  # Left
+    ]
+
+    for dr,dc in directions:
+      # Only visit cells that have same type of flower
+      if not is_valid_move(G, r + dr, c + dc) or G[r + dr][c + dc] != char:
+        continue
+
+      stack.append((r + dr, c + dc, G[r + dr][c + dc], component_parts))
+
+  # Calculate total with individual area * perimeters for each connected component
+  s = 0
+  for comps in components:
+    if comps != []:
+      sides = get_component_sides(G, comps)
+      
+      s += len(comps) * sides
+
+  print('Answer 2 is:', s)
 
 
 #############
@@ -115,4 +206,4 @@ print('\nSolution')
 print(40 * '=')
 
 part_one(inp)
-# part_two(inp)
+part_two(inp)
