97af7ac0095a88e8de604b565b54a2ba722aec46
Dominik <djessen94@gmail.com>
2024-12-18 15:54:44 +0100

    [Day 17] Part 1

--- 2024/day17/day17.py
+++ 2024/day17/day17.py
@@ -10,7 +10,49 @@ from utils.utils import *
 ############
 
 def part_one(inp: str):
-  print('Answer 1 is:')
+  combo_operands = {
+    "0": 0,
+    "1": 1,
+    "2": 2,
+    "3": 3,
+    "4": "A", # Register value
+    "5": "B", # Register value
+    "6": "C"  # Register value
+  }
+
+  r,p = inp.split('\n\n')
+  p = p.split(': ')[1].split(',')
+
+  for i, rg in enumerate(r.splitlines()):
+    combo_operands[str(i+4)] = int(rg.split(': ')[1])
+
+  out = []
+  i = 0
+  while i < len(p):
+    ins = p[i]
+    op = p[i+1]
+
+    match ins:
+      case "0":
+        combo_operands["4"] = int(combo_operands["4"] / 2 ** combo_operands[op])
+      case "1":
+        combo_operands["5"] ^= int(op)
+      case "2":
+        combo_operands["5"] = combo_operands[op] % 8
+      case "3":
+        i = i if combo_operands["4"] == 0 else int(op) - 2 
+      case "4":
+        combo_operands["5"] ^= combo_operands["6"]
+      case "5":
+        out.append(combo_operands[op] % 8)
+      case "6":
+        combo_operands["5"] = int(combo_operands["4"] / 2 ** combo_operands[op])
+      case "7":
+        combo_operands["6"] = int(combo_operands["4"] / 2 ** combo_operands[op])
+
+    i += 2
+
+  print('Answer 1 is:', ','.join(str(x) for x in out))
 
 
 ############
@@ -39,8 +81,8 @@ part_two(example)
 
 # Solve input
 
-# print('\nSolution')
-# print(40 * '=')
+print('\nSolution')
+print(40 * '=')
 
-# part_one(inp)
+part_one(inp)
 # part_two(inp)
