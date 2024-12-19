feb7650f825ee5f64a3e22f17e70c3011f3f1f4e
Dominik <djessen94@gmail.com>
2024-12-19 12:06:06 +0100

    [Day 19] Part 1

--- 2024/day19/day19.py
+++ 2024/day19/day19.py
@@ -1,5 +1,6 @@
 import sys
 import os
+from functools import cache
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 
@@ -10,7 +11,30 @@ from utils.utils import *
 ############
 
 def part_one(inp: str):
-  print('Answer 1 is:')
+  t,d = inp.split('\n\n')
+  t = set(t.split(', '))
+  largest_t = max(map(len, t))
+  d = d.splitlines()  
+
+  # Check longest possible towel first, then see if remaining towel design is possible
+  # Recurse and check smaller towels until no towel works (False) or design is made (True)
+  # Needs memoization to run in time (sub-designs of designs will always have the same answer)
+  @cache
+  def design_possible(d: str) -> bool:
+    if d == '':
+      return True
+    
+    for i in range(min(len(d), largest_t) + 1):
+      if d[:i] in t and design_possible(d[i:]):
+        return True
+      
+    return False
+
+  s = 0
+  for design in d:
+    s += design_possible(design)
+
+  print('Answer 1 is:', s)
 
 
 ############
@@ -39,8 +63,8 @@ part_two(example)
 
 # Solve input
 
-# print('\nSolution')
-# print(40 * '=')
+print('\nSolution')
+print(40 * '=')
 
-# part_one(inp)
+part_one(inp)
 # part_two(inp)
