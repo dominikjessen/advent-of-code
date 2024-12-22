4840516269245013526c1f9a08c86fcb43ef0acd
Dominik <djessen94@gmail.com>
2024-12-23 00:12:35 +0100

    [Day 20] Part 2

--- 2024/day20/day20.py
+++ 2024/day20/day20.py
@@ -75,7 +75,51 @@ def part_one(inp: str):
 ############
 
 def part_two(inp: str):
-  print('Answer 2 is:')
+  G = get_input_grid_char(inp)
+
+  sr, sc = 0, 0
+
+  for r in range(len(G)):
+    for c in range(len(G[r])):
+      if G[r][c] == 'S':
+        sr,sc = r,c
+  
+  # Build the track with its distance in picoseconds from start
+  track_secs = [[-1] * len(G[row]) for row in range(len(G))]
+  track_secs[sr][sc] = 0
+  cr,cc = sr,sc
+  while G[cr][cc] != 'E':
+    for nr,nc in get_adjacent_coords(cr,cc):
+      if not is_valid_move(G, nr, nc):
+        continue
+      if G[nr][nc] == '#':
+        continue
+      if track_secs[nr][nc] != -1:
+        continue
+      track_secs[nr][nc] = track_secs[cr][cc] + 1
+      cr,cc = nr,nc
+  
+  # Time to cheat massively now by checking all cells with a radius from current between 2 and 20 in distance
+  s = 0
+  for r in range(len(G)):
+    for c in range(len(G[r])):
+      if G[r][c] == '#':
+        continue
+      
+      # We're making a 20 radius diamond for cheating and only considering one "unique" cheat path if it saves enough time (100 + time to cheat)
+      for rad in range(2,21):
+        for dr in range(rad + 1):
+          dc = rad - dr
+          # Try all +/+, +/-, -/+, -/- options from here
+          for nr,nc in {(r + dr, c + dc), (r + dr, c - dc), (r - dr, c + dc), (r - dr, c - dc)}:
+            if not is_valid_move(G, nr,nc):
+              continue
+            if G[nr][nc] == '#':
+              continue
+            if track_secs[r][c] - track_secs[nr][nc] >= 100 + rad:
+              s += 1
+
+  print('Answer 2 is:', s)
 
 
 #############
@@ -100,4 +144,4 @@ print('\nSolution')
 print(40 * '=')
 
 part_one(inp)
-# part_two(inp)
+part_two(inp)
