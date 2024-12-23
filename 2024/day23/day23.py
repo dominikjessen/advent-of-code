83b1b71b95acb441b933daa78c813cf8e904b827
Dominik <djessen94@gmail.com>
2024-12-23 16:40:20 +0100

    [Day 23] Part 2

--- 2024/day23/day23.py
+++ 2024/day23/day23.py
@@ -28,7 +28,7 @@ def part_one(inp: str):
           add = [k, c[0], c[1]]
           add.sort()
           candidates.add((add[0], add[1], add[2]))
-          
+
   print('Answer 1 is:', len(candidates))
 
 
@@ -37,7 +37,33 @@ def part_one(inp: str):
 ############
 
 def part_two(inp: str):
-  print('Answer 2 is:')
+  # I need edges to be a set for my operations, so no networkx this time 🤷🏻‍♂️
+  edges = {}
+  for l in inp.splitlines():
+    a,b = l.split('-')
+    if a not in edges:
+      edges[a] = set()
+    if b not in edges:
+      edges[b] = set()
+    edges[a].add(b)
+    edges[b].add(a)
+
+  candidates = set()
+
+  def connect_node(node: str, fully_connected: Set[str]):
+    k = tuple(sorted(fully_connected))
+    if k in candidates:
+      return
+    candidates.add(k)
+    for neighbor in edges[node]:
+      if neighbor not in fully_connected and fully_connected <= edges[neighbor]:
+        # Check the next node with a union of the node we just added
+        connect_node(neighbor, fully_connected | {neighbor})
+
+  for n in edges:
+    connect_node(n, {n})
+
+  print('Answer 2 is:', ','.join(sorted(max(candidates, key=len))))
 
 
 #############
@@ -62,4 +88,4 @@ print('\nSolution')
 print(40 * '=')
 
 part_one(inp)
-# part_two(inp)
+part_two(inp)
