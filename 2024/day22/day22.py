dbeca6a19af7250714f494fbe6c9bf0e8c307eec
Dominik <djessen94@gmail.com>
2024-12-23 17:36:02 +0100

    [Day 22] Part 1

--- 2024/day22/day22.py
+++ 2024/day22/day22.py
@@ -1,3 +1,4 @@
+from math import floor
 import sys
 import os
 
@@ -9,8 +10,23 @@ from utils.utils import *
 #  Part 1  #
 ############
 
+def transform_num(n: int) -> int:
+  n = (n ^ (n * 64)) % 16777216
+  n = (n ^ (n // 32)) % 16777216
+  n = (n ^ (n * 2048)) % 16777216
+
+  return n
+
 def part_one(inp: str):
-  print('Answer 1 is:')
+  nums = list(map(int, get_input_rows(inp)))
+
+  s = 0
+  for n in nums:
+    for _ in range(2000):
+      n = transform_num(n)
+    s += n
+
+  print('Answer 1 is:', s)
 
 
 ############
@@ -39,8 +55,8 @@ part_two(example)
 
 # Solve input
 
-# print('\nSolution')
-# print(40 * '=')
+print('\nSolution')
+print(40 * '=')
 
-# part_one(inp)
+part_one(inp)
 # part_two(inp)
