0f78623400cb4e21f6a42d640af36c656d051ba6
Dominik <djessen94@gmail.com>
2024-12-25 21:52:57 +0100

    [Day 24] Part 2

--- 2024/day24/day24.py
+++ 2024/day24/day24.py
@@ -67,9 +67,129 @@ def part_one(inp: str):
 #  Part 2  #
 ############
 
-def part_two(inp: str):
-  print('Answer 2 is:')
+# Time to build some binary addition module with carry
+#
+#      1 0 1 1 (11)
+# +    0 1 1 1 (7)
+# ----------------
+# C  1 1 1 1 -
+# =================
+#    1 0 0 1 0 (18)
 
+# And the final answer bit is essentially a carry bit, so it'll still fail, but we will have found our solution by then
+def nth_wire_str(wire_char: str, bit: int) -> str:
+  return f"{wire_char}{bit:02}"
+
+def part_two(inp: str):
+  value_gates = {}
+  conns = {}
+
+  starts, wirings = inp.split('\n\n')
+  for l in starts.splitlines():
+    g,v = l.split(': ')
+    value_gates[g] = int(v)
+  
+  for conn in wirings.splitlines():
+    a,op,b,_,t = conn.split(' ')
+
+    conns[t] = (a,op,b)
+  
+  def assert_result(w: str, n: int) -> bool:
+    print('asserting result', w, n)
+    a,op,b = conns[w]
+    if op != 'XOR':
+      return False
+    
+    if n == 0:
+      return sorted([a,b]) == ['x00', 'y00']
+
+    return assert_xor(a, n) and assert_carry(b, n) or assert_xor(b,n) and assert_carry(a,n)
+
+  def assert_xor(w: str, n: int) -> bool:
+    print('asserting XOR', w, n)
+    a,op,b = conns[w]
+
+    if op != 'XOR':
+      return False
+  
+    return sorted([a,b]) == [f"{'x'}{n:02}", f"{'y'}{n:02}"]
+  
+  # 0th bit has no carry
+  # 1st carry bit has 2 dependent bits determining it
+  # All subsequent carry bits have 3 inputs (the a, b bits' nth values and the nth carry)
+  def assert_carry(w: str, n: int) -> bool:
+    print('asserting carry', w, n)
+    a,op,b = conns[w]
+    
+    if n == 1:
+      if op != 'AND':
+        return False
+      return sorted([a,b]) == ['x00', 'y00']
+    
+    if op != 'OR':
+      return False
+    
+    return assert_direct_carry(a, n - 1) and assert_recarry(b, n - 1) or assert_direct_carry(b, n - 1) and assert_recarry(a, n - 1)
+  
+  def assert_direct_carry(w: str, n: int) -> bool:
+    print('asserting direct carry', w, n)
+    a,op,b = conns[w]
+    
+    if op != 'AND':
+      return False
+    
+    return sorted([a,b]) == [f"{'x'}{n:02}", f"{'y'}{n:02}"]
+  
+  def assert_recarry(w: str, n: int) -> bool:
+    print('asserting re-carry', w, n)
+    a,op,b = conns[w]
+    
+    if op != 'AND':
+      return False
+    
+    return assert_xor(a, n) and assert_carry(b, n) or assert_xor(b,n) and assert_carry(a,n)
+
+  # Now we start looping through all our z-bits and manually fix the errors
+  i = 0
+  while True:
+    if not assert_result(f"{'z'}{i:02}", i):
+      break
+    i += 1
+
+  print('Wrong wiring for', f"{'z'}{i:02}")
+  swaps = ['bpf', 'fdw', 'hcc', 'hqc', 'qcw', 'z05', 'z11', 'z35']
+  print('Answer 2 is:', ','.join(swaps))
+
+# Manual solution
+# z05 <> bpf
+# z11 <> hcc
+# hqc <> qcw
+# z35 <> fdw
+
+# Explanations
+# -------------------------------
+# z05 wrong! Let's find the issue
+# Last debug is: asserting result z05 5 ==> error must lie with the OP for a op b --> z05. Let's check the input!
+# Line: qfs AND whh -> z05 ==> This can't be correct! z05 needs to be an XOR. Let's find qfs and whh
+# Line: x05 XOR y05 -> qfs ==> This is the carry bit. This one seems good.
+# Line: rcr OR fkc -> whh ==> This can be a valid intermediate and we know op is wrong. Let's find qfs XOR whh
+# Line: qfs XOR whh -> bpf ==> qfs XOR whh MUST be z05. So let's swap bpf and z05
+
+# z11 wrong -- skn OR spp -> z11
+# skn XOR spp doesn't exist ==> Error must be downstream
+# Line: cjh AND cgn -> skn ==> this seems like it could feasibly lead us to our result skipping skn and spp
+# Line: x11 AND y11 -> spp ==> correct
+# Line: cgn XOR cjh -> hcc ==> culprit is hcc. Let's swap hcc and z11
+
+# z24 wrong -- gmr XOR hqc -> z24 ==> This seems feasible, so look downstream:
+# Last debugs are: asserting XOR gmr 24 & asserting XOR hqc 24
+# Line: wkq OR stf -> gmr ==> seems good enough, if other one is XOR
+# Line: x24 AND y24 -> hqc ==> This is the issue it needs to be the XOR
+# Line: y24 XOR x24 -> qcw ==> Swap qcw with hqc
+
+# z35 wrong -- x35 AND y35 -> z35 ==> This is accidentally the intermediate bit, so let's find something by starting with x35 XOR y35
+# Line: y35 XOR x35 -> khk ==> This is still fine, so check where khk is used
+# Line: khk XOR tgs -> fdw ==> This can't be an XOR, needs to be AND or OR because of what khk is ==> swap fdw with z35
 
 #############
 #  Solving  #
@@ -85,7 +205,6 @@ print('Example')
 print(40 * '=')
 
 part_one(example)
-part_two(example)
 
 # Solve input
 
@@ -93,4 +212,4 @@ print('\nSolution')
 print(40 * '=')
 
 part_one(inp)
-# part_two(inp)
+part_two(inp)
